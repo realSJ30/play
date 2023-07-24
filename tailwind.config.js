cacc5228b7adace7ea4bbdf7c4ee20d38655948d
realSJ30 <realsjmoraga@gmail.com>
2023-07-24 15:46:42 +0800

    feat: initial commit/added template of the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors:{
+        'secondary': '#1A0F3B'
+      }
     },
   },
   plugins: [],
